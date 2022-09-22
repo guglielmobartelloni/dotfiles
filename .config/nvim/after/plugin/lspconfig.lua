3081ec0a9408c72bb3b68625ebd72a25ad7d53f2
samoorai <hirotakatech@gmail.com>
2022-09-22 16:42:56 +0200

    Sync

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -40,9 +40,9 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
+            -- if cmp.visible() then
+            --     cmp.select_next_item()
+            if luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
             elseif has_words_before() then
                 cmp.complete()
