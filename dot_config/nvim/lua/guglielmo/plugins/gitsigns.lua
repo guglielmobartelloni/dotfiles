f36c62a91d8a331cf0306f858544044e6d1c22f2
samoorai <hirotakatech@gmail.com>
2024-01-08 16:57:49 +0100

    add gitsigns keymap

--- dot_config/nvim/lua/guglielmo/plugins/gitsigns.lua
+++ dot_config/nvim/lua/guglielmo/plugins/gitsigns.lua
@@ -1,5 +1,8 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = true,
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = true,
+    keys = {
+        { "<leader>gb", "<CMD>Gitsigns toggle_current_line_blame<CR>" }
+    }
 }
