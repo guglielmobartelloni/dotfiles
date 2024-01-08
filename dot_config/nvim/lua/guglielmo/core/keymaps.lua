1e596b9f3565c2504ad5e35f3bb1780f2c4912ed
samoorai <hirotakatech@gmail.com>
2024-01-08 15:17:59 +0100

    Added new useful keymaps

--- dot_config/nvim/lua/guglielmo/core/keymaps.lua
+++ dot_config/nvim/lua/guglielmo/core/keymaps.lua
@@ -1,6 +1,19 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
+-- Define a variable to keep track of the toggle state
+local isCommandEnabled = false
+-- Define the command you want to toggle
+function ToggleQuickFix()
+    if isCommandEnabled then
+        vim.cmd("cclose")
+    else
+        vim.cmd("copen")
+    end
+    -- Toggle the state
+    isCommandEnabled = not isCommandEnabled
+end
+
 local keymap = vim.keymap -- for conciseness
 
 ---------------------
@@ -8,6 +21,10 @@ local keymap = vim.keymap -- for conciseness
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- for visual editing
+keymap.set("n", "j", "gj", { desc = "" })
+keymap.set("n", "k", "gk", { desc = "" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
@@ -20,9 +37,9 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 keymap.set("n", "<C-d>", "<C-d>zz")
@@ -31,9 +48,12 @@ keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
 
 -- Quickfix list
-keymap.set("n", "<leader>q", ":copen<CR>", { desc = "Open quickfix list" })
+keymap.set("n", "<leader>q", ":lua ToggleQuickFix()<CR>", { desc = "Open quickfix list" })
 keymap.set("n", "[q", ":cprev<CR>", { desc = "Prev quickfix" })
 keymap.set("n", "]q", ":cnext<CR>", { desc = "Next quickfix" })
+
+keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clip" })
+keymap.set("n", "<leader>y", '"+y', { desc = "Copy from system clip" })
 
 -- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 -- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
