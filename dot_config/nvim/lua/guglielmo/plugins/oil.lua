96758444fa95f50435a139fc9b15ea835e7f2a69
samoorai <hirotakatech@gmail.com>
2024-01-09 12:09:09 +0100

    improve oil configuration, start instead of netrw

--- dot_config/nvim/lua/guglielmo/plugins/oil.lua
+++ dot_config/nvim/lua/guglielmo/plugins/oil.lua
@@ -1,9 +1,22 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    {"-", "<CMD>Oil<CR>"}
-  }
+    'stevearc/oil.nvim',
+    dependencies = {
+        { 'nvim-treesitter/nvim-treesitter' },
+        { 'nvim-tree/nvim-web-devicons',    lazy = true },
+    },
+    opts = {
+        default_file_explorer = true,
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, _)
+                return vim.startswith(name, '.DS_Store')
+            end,
+        },
+        float = { padding = 4 },
+    },
+    config = function(_, opts)
+        local oil = require('oil')
+        oil.setup(opts)
+        vim.keymap.set('n', '-', oil.open, { desc = 'Open parent directory' })
+    end
 }
