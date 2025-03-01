a2e1adb81391a4a831fe1390ada9c4b207b01a9e
guglielmobartelloni <bartelloni.guglielmo@gmail.com>
2025-03-01 18:16:14 +0100

    feat: add toggle terminal

--- nvim/init.lua
+++ nvim/init.lua
@@ -834,3 +834,40 @@ local function map(mode, lhs, rhs, opts)
 end
 
 map('n', '<leader>jt', ':JavaTestRunCurrentClass<CR>')
+local te_buf = nil
+local te_win_id = nil
+
+local v = vim
+local fun = v.fn
+local cmd = v.api.nvim_command
+local gotoid = fun.win_gotoid
+local getid = fun.win_getid
+
+local function openTerminal()
+	if fun.bufexists(te_buf) ~= 1 then
+		cmd("au TermOpen * setlocal nonumber norelativenumber signcolumn=no")
+		cmd("sp | winc J | res 10 | te")
+		te_win_id = getid()
+		te_buf = fun.bufnr('%')
+	elseif gotoid(te_win_id) ~= 1 then
+		cmd("sb " .. te_buf .. "| winc J | res 10")
+		te_win_id = getid()
+	end
+	cmd("startinsert")
+end
+
+local function hideTerminal()
+	if gotoid(te_win_id) == 1 then
+		cmd("hide")
+	end
+end
+
+function ToggleTerminal()
+	if gotoid(te_win_id) == 1 then
+		hideTerminal()
+	else
+		openTerminal()
+	end
+end
+
+vim.keymap.set('n', '<leader>t', ToggleTerminal, { desc = 'Toggle terminal' })
