df137f7d45845c1caabec34bc66ca5150c545cd4
guglielmobartelloni <bartelloni.guglielmo@gmail.com>
2025-06-30 18:16:24 +0200

    feat: change font size and vim window resizing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -75,6 +75,7 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
@@ -110,6 +111,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '==', '<C-w>=')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
