fc29f4e80122d3765e7663ba523866473ee7a148
Guglielmo Bartelloni <samurai@MacBook-Pro-di-Guglielmo.local>
2025-07-10 18:52:59 +0200

    feat: add whatsapp messages script

---
+++ .config/sketchybar/plugins/whatsapp.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# AppleScript to fetch WhatsApp Dock badge count
+UNREAD=$(osascript <<'APPLESCRIPT'
+set total to 0
+tell application "System Events"
+  tell process "Dock"
+    try
+      set unread to value of attribute "AXStatusLabel" of UI element "WhatsApp" of list 1
+      if unread is not missing value then
+        set total to unread as integer
+      end if
+    end try
+  end tell
+end tell
+return total
+APPLESCRIPT
+)
+
+if [[ $UNREAD -gt 0 ]]; then
+  sketchybar --set "$NAME" label="$UNREAD" icon= 
+else
+  sketchybar --set "$NAME" label="" icon=
+fi
